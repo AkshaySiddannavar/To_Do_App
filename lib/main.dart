a702bed64e31ad69b9a74cddf331fcd4076fc9e0
Akshay Siddannavar <aeonic619@gmail.com>
2022-04-29 23:00:41 +0530

    add basic structure of todo app without functionality
    added basic structure of todo app to make it look like mockup without functionality, functionality
    will be added in future

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/task_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
