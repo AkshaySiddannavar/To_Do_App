ec6dfcae79bcdd24c6a1894288f153db94129702
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-02 15:34:39 +0530

    add functionality to checkbox
    Added functionality to chekcboxes using local state. Striking off of text
    in Text() when checkbox is ticked is yet to be done.

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -7,7 +7,28 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('Buy Milk'),
-      trailing: Checkbox(value: false, onChanged: (newValue) {}),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = !isChecked;
+        });
+      },
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
