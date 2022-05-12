895c7d110daae2a10587f3ce503eb43ebb9f4c08
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-12 12:58:00 +0530

    make code camera-ready
    Removed debug-mode-banner. Changed position of FAB to center. Initial
    value of _tasks has been set to empty list.

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => AllTaskData(),
       child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: TasksScreen(),
       ),
     );
--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -15,6 +15,7 @@ class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
